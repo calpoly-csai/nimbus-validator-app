66ad0ac17bf6c4249052b7d037bb28ef0c5ebdd2
Mason McElvain <52104630+masonmcelvain@users.noreply.github.com>
2020-06-23 16:52:02 -0700

    Remove unnecessary import

--- src/pages/Validator/Validator.js
+++ src/pages/Validator/Validator.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import "./Validator.scss";
 import ValidatorForm from "./ValidatorForm";
 import TokenBar from "./TokenBar";
