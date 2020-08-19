1cf0af95b6b4439bcb50c25b243f085866248abd
Mason McElvain <masonmcelvain@users.noreply.github.com>
2020-08-19 07:46:49 -0700

    Letters fall into place left to right

--- src/components/LoginHero/LoginHero.js
+++ src/components/LoginHero/LoginHero.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef } from 'react';
 import './LoginHero.scss';
 
-const d = 300
+const d = 300;
 
 function randomDist(dist) {
     return (Math.random() * 2 - 1) * dist
@@ -14,33 +14,37 @@ export default function LoginHero() {
                     "Nimbus, take me to your data",
                     "Let's clean some data",
                     "CS + AI = best club ever"];
-    let index = Math.floor(Math.random() * messages.length)
-    let messageRef = useRef(null)
+    let index = Math.floor(Math.random() * messages.length);
+    let messageRef = useRef(null);
     let currentMessage = messages[index].split('').map((char, i) => <span className='letter' style={{
         display: `${char === ' ' ? 'initial' : ''}`
-    }} key={i} > {char}</ span >)
+    }} key={i} > {char}</ span >);
 
     let animateLetters = () => {
-        let letters = [...messageRef.current.children]
+        let letters = [...messageRef.current.children];
+        let i = 0;
         letters.forEach(letter => {
+            let delay = i * 100;
             // animate each individual span element (character)
             let keyframes = {
-                transform: [`translate(${1 * randomDist(d)}px, ${1 * randomDist(d)}px)`, 
+                transform: [
                 `translate(${0.8911 * randomDist(d)}px, ${0.8911 * randomDist(d)}px)`,
                 `translate(${0.5644 * randomDist(d)}px, ${0.5644 * randomDist(d)}px)`,
                 `translate(${0.0299 * randomDist(d)}px, ${0.0199 * randomDist(d)}px)`,
                 `translate(${0.0066 * randomDist(d)}px, ${0.0066 * randomDist(d)}px)`,
                 `translate(${0.0163 * randomDist(d)}px, ${0.0163 * randomDist(d)}px)`,
                 `translate(${0.0154 * randomDist(d)}px, ${0.0154 * randomDist(d)}px)`,
-                `translate(0, 0)`],
+                'translate(0, 0)'],
                 opacity: ['0.2', '1']
-            }
+            };
             // easing function: easeInOutSine https://easings.net/#easeInOutSine
-            letter.animate(keyframes, { duration: 4000, easing: 'cubic-bezier(0.37, 0, 0.63, 1)' })
-        })
+            letter.animate(keyframes, { duration: 4000, easing: 'cubic-bezier(0.37, 0, 0.63, 1)', delay: delay});
+            setTimeout(() => {letter.style.opacity = 1}, delay + 100);
+            i++;
+        });
     }
 
-    useEffect(animateLetters, [messageRef])
+    useEffect(animateLetters, [messageRef]);
 
     return (
         <div className="LoginHero">
