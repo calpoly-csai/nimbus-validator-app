a538d18d3fabf23e421987c52e6ecacf2c347666
Mason McElvain <masonmcelvain@users.noreply.github.com>
2020-08-26 12:51:25 -0700

    Replace input with attribute name on click

--- src/pages/Validator/AutocompleteList.js
+++ src/pages/Validator/AutocompleteList.js
@@ -8,48 +8,61 @@ export default function AutocompleteList({
   onSelect,
   onCreate,
 }) {
-  // Calls onSelect to insert token into the UI
+  /*
+   * Inserts token into the UI, replacing whatever text the user already wrote 
+   * in the token
+   */
   let handleSelect = (entity) => {
     onSelect(entity);
   };
 
-  let entitiesSeen = [];
+  
+  /*
+   * Returns a list of synonym autocomplete options, filtered by the input
+   */
+  let listSynonyms = () => {
+    let entitiesSeen = [];
+    return (
+      Object.keys(synonyms)
+      .filter(entity => entity.toUpperCase().startsWith(inputVal.toUpperCase()))
+      .map((entity) => {
+        if (!entitiesSeen.includes(synonyms[entity])) {
+          entitiesSeen.push(synonyms[entity])
+          return (
+            <li key={entity} onClick={handleSelect.bind(this, synonyms[entity])} >
+              {synonyms[entity]}
+            </li >
+          )
+        }
+      })
+    );
+  };
 
-  // Create a list of synonyms, filter them with the input value, and then return a token if the entity isn't already displayed
-  let showOptions = Object.keys(synonyms)
-    .filter((entity) => entity.toUpperCase().startsWith(inputVal.toUpperCase()))
-    .map((entity) => {
-      if (!entitiesSeen.includes(synonyms[entity])) {
-        entitiesSeen.push(synonyms[entity])
-        return (
-          <li key={entity} onClick={handleSelect.bind(this, synonyms[entity])} >
-            {synonyms[entity]}
-          </li >
-        )
-      }
-    });
-
-    // TODO: filter the attribute names
-    // TODO: fix the onclick handler
+  /*
+   * Returns a list of attribute autocomplete options, filtered by the input
+   */
   let listAttributes = () => {
     let entity = showAttributes;
+    let inputAttr = inputVal.match(/(?<=\.).*/);
     if(!entities[entity]){
       return
     }
-    return entities[entity]['attributes'].map((attr) => {
+    return entities[entity]['attributes']
+      .filter((attr) => attr.includes(inputAttr))
+      .map((attr) => {
       return (
-        <li key={attr} onClick={handleSelect.bind(this, synonyms[entity])} >
+        <li key={attr} onClick={handleSelect.bind(this, `${entity}.${attr}`)} >
           {attr}
         </li >
       )
     })
   }
-  
-  if (showAttributes) {
-    showOptions = listAttributes()
-  }
 
-  // Render the list of tokens, if not display an add token button
+  let showOptions = showAttributes ? listAttributes() : listSynonyms();
+
+  /* 
+   * Render the list of tokens, if not display an add token button
+   */
   return (
     <ul className="AutocompleteList">
       {showOptions.length ? (
